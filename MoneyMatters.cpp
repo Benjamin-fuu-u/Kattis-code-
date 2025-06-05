715e341f337d2d501042724280434aea7556fc00
Benjamin-fuu-u <gzfu422@gmail.com>
2025-06-05 10:15:21 +0800

    Update MoneyMatters.cpp

--- MoneyMatters.cpp
+++ MoneyMatters.cpp
@@ -11,18 +11,18 @@ int main（）
     cin>>friend;
     for(int i=1 ;i<=people;i++)
     {
-			int k;
-			cin>>k;
-			money[i]=k;
-		}
+	int k;
+	cin>>k;
+	money[i]=k;
+    }
     for(int i=0 i<friend; i++)
-		{
-			int a,b;
-			cin>>a>>b;
-			connect[a].push_back(b);
-			connect [b].push_back(a)
-		}
- fjkdjdkdjd
+    {
+	int a,b;
+	cin>>a>>b;
+	connect[a].push_back(b);
+	connect [b].push_back(a);
+     }
+    
 
 		 
  ｝

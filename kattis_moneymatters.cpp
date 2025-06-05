def6213bfdf5c74de385ed480ac092069ac750d0
Benjamin-fuu-u <gzfu422@gmail.com>
2025-06-05 09:52:38 +0800

    Create kattis_moneymatters.cpp

---
+++ kattis_moneymatters.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+map<int,int> money;
+map<int,vector<int>> connect;
+int main（）
+｛
+    int people ;
+    cin>>people;
+    int friend;
+    cin>>friend;
+    for(int i=1 ;i<=people;i++)
+    {
+			int k;
+			cin>>k;
+			money[i]=k;
+		}
+    for(int i=0 i<friend; i++)
+		{
+			
+		}
+
+
+		 
+ ｝
